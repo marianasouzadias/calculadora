d4175dcf68cfb4ac8750085b71a67f7030d92e3f
Mariana <mariana.mkt.dias@gmail.com>
2021-12-07 20:26:54 -0300

    primeiro comiti

--- calculadora.js
+++ calculadora.js
@@ -1,1 +1,20 @@
-console.log('calculadora');
+function calculadora (num1, num2, operacao) {
+    if (operacao == "+"){
+     return num1 + num2;
+    }else if (operacao == "-"){
+       return num1 - num2;
+    }else if (operacao == "*") {
+       return num1 * num2;
+    }else if (operacao == "/") {
+         return num1 / num2;
+      } else {
+       return "Não definido";
+     }
+    };
+    
+    
+    console.log(calculadora(10, 2, "+"));
+     console.log(calculadora(10, 2, "-"));
+      console.log(calculadora(10, 2, "*"));
+       console.log(calculadora(10, 2, "/"));
+        console.log(calculadora(10, 2,));
